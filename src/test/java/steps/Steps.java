af80135369d3fd547e507840f5a00ca591164009
Your Name <tatiane031315@gmail.com>
2022-05-24 15:48:48 -0300

    api1.2

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -6,24 +6,24 @@ import io.cucumber.java.en.When;
 import metodos.Metodos;
 
 public class Steps {
-
-	Metodos m = new Metodos();
 	
+	Metodos m = new Metodos();
+
 	@Given("retornar a consulta {string}")
-	public void retornar_a_consulta(String cep) {
-	   m.GetCepValido(cep);
+	public void retornar_a_consulta(String string) {
+		m.GetCepValido(string);
+	    
 	}
 
 	@When("retornar a consulta")
 	public void retornar_a_consulta() {
-	    m.ConsultaCep();
+	   
 	}
 
 	@Then("sera exibido as informacoes do cep")
 	public void sera_exibido_as_informacoes_do_cep() {
-	 
+	    
 	}
-	
 	
 	
 	
--- target/classes/api.feature
+++ target/classes/api.feature
@@ -1,16 +1,20 @@
-
 @tag
-Feature: servico de consulta de cep
-@tag1
-Scenario Outline: Consulta Cep Valido
+Feature: Title of your feature
+I want to use this template for my feature file
+
+
+
+
+@tag2
+Scenario Outline: servico de consulta de cep
 Given retornar a consulta "<cep>"
 When retornar a consulta
 Then sera exibido as informacoes do cep
 
 
 
-Examples: 
-
-|cep|
-|448900001|
-|44900000|
+Examples:
+| cep |
+|47807012 |
+|47800-145|
+|47800-632|
